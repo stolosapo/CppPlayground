363e813d28f3f117500639ae5d1b4443ed07948b
Apostolis <aldrigmej@yahoo.gr>
2015-07-06 23:23:03 +0300

    Created container for euler's problems

---
+++ src/euler/EulerProblemContainer.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <string>
+#include "EulerProblemContainer.h"
+#include "Problem001.cpp"
+
+using namespace std;
+
+EulerProblemContainer::EulerProblemContainer()
+{
+	const int SIZE = 1;
+	this->size = SIZE;
+	this->problems = new EulerProblem *[SIZE];
+
+	fillProblems();
+}
+
+EulerProblemContainer::~EulerProblemContainer()
+{
+	delete this->problems;
+}
+
+void EulerProblemContainer::fillProblems()
+{
+	this->problems[0] = new Problem001;
+}
+
+void EulerProblemContainer::showProblems()
+{
+	for (int i = 0; i < size; ++i)
+	{
+		EulerProblem *current = this->problems[i];
+		cout << current->getId() << ". " 
+			<< current->getName() << endl;
+	}
+
+	cout << endl << -1 << ". Exit";
+}
+
+EulerProblem *EulerProblemContainer::findProblem(int id)
+{
+	if (id == -1)
+		return NULL;
+
+	EulerProblem *current;
+
+	for (int i = 0; i < this->size; ++i)
+	{
+		current = this->problems[i];
+		if (current->getId() == id)
+		{
+			return current;
+		}
+	}
+
+	cout << "No such problem exists!!!" << endl << endl;
+	cout << "Choose again: ";
+	cin >> id;
+	return this->findProblem(id);
+}
+
+void EulerProblemContainer::run()
+{
+	int id;
+	this->showProblems();
+
+	cout << endl << "Choose a problem: ";
+	cin >> id;
+
+	EulerProblem *selected = this->findProblem(id);
+	if (selected != NULL)
+	{
+		cout << selected->identify();
+		selected->findSolution();
+	}
+}
--- src/main.cpp
+++ src/main.cpp
@@ -4,21 +4,19 @@
 #include <stdio.h>
 #include "log/LogServiceFactory.h"
 #include "time/TimeServiceFactory.h"
-#include "euler/Problem001.cpp"
+// #include "euler/Problem001.cpp"
+#include "euler/EulerProblemContainer.h"
 
 using namespace std;
 
 int main()
 {
-	ILogService *srv = LogServiceFactory::create();
+	EulerProblemContainer container;
+	container.run();
+
+	// ILogService *srv = LogServiceFactory::create();
 	// srv->test();
 	
-	Problem001 prob001;
-	srv->info(prob001.identify());
-
-	prob001.findSolution();
-	
-
 	// srv->info("This is an information");
 	// srv->debug("This is an debug information");
 	// srv->error("This is an error");
