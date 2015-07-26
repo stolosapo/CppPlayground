62a934c61e769572995ee20b06c435cb54ebc272
Apostolis <aldrigmej@yahoo.gr>
2015-07-26 20:31:30 +0300

    Menu: created infrastructure for menu action

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,4 @@
 #include <iostream>
-#include <string>
-#include <time.h>
-#include <stdio.h>
-#include "log/LogServiceFactory.h"
-#include "time/TimeServiceFactory.h"
-#include "menu/MenuFactory.h"
-#include "euler/EulerProblemContainer.h"
 #include "menu/MainMenu.cpp"
 
 using namespace std;
