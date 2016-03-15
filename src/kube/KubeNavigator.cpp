04aea3c9ffe23caa5c05958611bb23b8517795f3
Apostolis <aldrigmej@yahoo.gr>
2016-03-15 22:54:56 +0200

    Changes in navigator

---
+++ src/kube/KubeNavigator.cpp
@@ -0,0 +1,95 @@
+#include "KubeNavigator.h"
+
+KubeNavigator::KubeNavigator(Kube *kube)
+{
+	this->kube = kube;
+}
+
+KubeNavigator::~KubeNavigator()
+{
+
+}
+
+
+void KubeNavigator::front()
+{
+
+}
+
+void KubeNavigator::back()
+{
+
+}
+
+void KubeNavigator::up()
+{
+
+}
+
+void KubeNavigator::left()
+{
+
+}
+
+void KubeNavigator::right()
+{
+
+}
+
+
+void KubeNavigator::moveX()
+{
+
+}
+
+void KubeNavigator::moveY()
+{
+
+}
+
+void KubeNavigator::moveZ()
+{
+
+}
+
+
+void KubeNavigator::aFront()
+{
+
+}
+
+void KubeNavigator::aBack()
+{
+
+}
+
+void KubeNavigator::aUp()
+{
+
+}
+
+void KubeNavigator::aLeft()
+{
+
+}
+
+void KubeNavigator::aRight()
+{
+
+}
+
+
+void KubeNavigator::moveAx()
+{
+
+}
+
+void KubeNavigator::moveAy()
+{
+
+}
+
+void KubeNavigator::moveAz()
+{
+
+}
--- src/kube/KubeNavigator.h
+++ src/kube/KubeNavigator.h
@@ -3,9 +3,32 @@
 
 class KubeNavigator
 {
+private:
+	Kube *kube;
+
 public:
-	KubeNavigator();
+	KubeNavigator(Kube *kube);
 	virtual ~KubeNavigator();
+
+	void front();
+	void back();
+	void up();
+	void left();
+	void right();
+
+	void moveX();
+	void moveY();
+	void moveZ();
+
+	void aFront();
+	void aBack();
+	void aUp();
+	void aLeft();
+	void aRight();
+
+	void moveAx();
+	void moveAy();
+	void moveAz();
 };
 
 #endif // KubeNavigator_h__
