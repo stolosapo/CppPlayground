a3f452ebbe0c5f9d99461a26166045afd67187c5
Apostolis <aldrigmej@yahoo.gr>
2016-06-10 20:01:07 +0300

    dataModel: create first class

---
+++ src/dataModel/Model.h
@@ -0,0 +1,24 @@
+#ifndef Model_h__
+#define Model_h__
+
+#include <iostream>
+#include <string>
+#include <map>
+
+#include "Field.h"
+
+using namespace std;
+
+class Model
+{
+private:
+	
+
+protected:
+
+
+public:
+	Model();
+	virtual ~Model();
+
+};
