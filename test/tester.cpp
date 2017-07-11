3f843a123014a172f643cb39f98d8b7e5604bdb0
Apostolos <apostolos@projectbeagle.com>
2017-07-11 11:26:17 +0300

    Fix warnings

--- test/tester.cpp
+++ test/tester.cpp
@@ -42,10 +42,15 @@ void test_arg_parser_executable_name()
 	int argc = SIZE;
 	char* argv[SIZE];
 
-	argv[0] = "./executable";
-	argv[1] = "first";
-	argv[2] = "second";
-	argv[3] = "third";
+	const char *arg0 = "./executable";
+	const char *arg1 = "first";
+	const char *arg2 = "second";
+	const char *arg3 = "third";
+
+	argv[0] = (char*) arg0;
+	argv[1] = (char*) arg1;
+	argv[2] = (char*) arg2;
+	argv[3] = (char*) arg3;
 
 	ArgParser* parser = new ArgParser(argc, argv);
 	parser->parse();
