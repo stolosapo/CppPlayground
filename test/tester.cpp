09b63da8e1540ed7cd36757912fa2f0f155348ba
Apostolis <aldrigmej@yahoo.gr>
2017-07-12 21:44:18 +0300

    Code clean up

--- test/tester.cpp
+++ test/tester.cpp
@@ -1,22 +1,9 @@
 #include <iostream>
 #include <stdlib.h>
 
-#include "testLib/UnitTest.h"
-#include "testLib/Assertion.h"
-
-#include "../src/arguments/ArgParser.h"
-
-#include "unitTests/arguments/ArgParserTest.h"
-
 #include "unitTests/MainTestSuite.h"
 
 using namespace std;
-
-
-void ena_aplo_true_true_test();
-void ena_aplo_true_false_test();
-void ena_aplo_false_true_test();
-void ena_aplo_false_false_test();
 
 
 int main(int argc, char* argv[])
@@ -26,13 +13,6 @@ int main(int argc, char* argv[])
 	cout << endl ;
 	cout << "Starting Unit Tests.." << endl ;
 	cout << endl ;
-
-	// passed &= assertMethod("Ena aplo TRUE - TRUE test", &ena_aplo_true_true_test);
-	// passed &= assertMethod("Ena aplo TRUE - FALSE test", &ena_aplo_true_false_test);
-	// passed &= assertMethod("Argument Parser: Test Executable Name", &test_arg_parser_executable_name);
-	// passed &= assertMethod("Ena aplo FALSE - TRUE test", &ena_aplo_false_true_test);
-	// passed &= assertMethod("Ena aplo FALSE - FALSE test", &ena_aplo_false_false_test);
-
 
 	MainTestSuite* ttt = new MainTestSuite;
 
@@ -46,24 +26,4 @@ int main(int argc, char* argv[])
 	{
 		exit(EXIT_FAILURE);
 	}
-}
-
-void ena_aplo_true_true_test()
-{
-	assertTrue(true);
-}
-
-void ena_aplo_true_false_test()
-{
-	assertFalse(false);
-}
-
-void ena_aplo_false_true_test()
-{
-	assertTrue(false);
-}
-
-void ena_aplo_false_false_test()
-{
-	assertFalse(true);
 }
