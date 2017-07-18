78175dede9413a59330013aa448abb4b063c96f8
Apostolos <apostolos@projectbeagle.com>
2017-07-18 14:59:13 +0300

    Added more unit tests for Function

--- test/unitTests/lib/functional/FunctionTest.cpp
+++ test/unitTests/lib/functional/FunctionTest.cpp
@@ -19,12 +19,15 @@ void FunctionTest::registerTests()
 {
 	registerCoveredMethod("func");
 	registerCoveredMethod("func_param_pointer");
-	// registerCoveredMethod("func_return_pointers");
-	// registerCoveredMethod("func_all_pointers");
+	registerCoveredMethod("func_return_pointers");
+	registerCoveredMethod("func_all_pointers");
 
 	registerTest("Function with int", &test_function_with_int_apply);
 	registerTest("Function with int to double", &test_function_with_double_int_apply);
+
 	registerTest("Function with param int pointer to int", &test_function_with_func_param_int_pointer);
+	registerTest("Function with param int to int pointer", &test_function_with_return_pointer_apply);
+	registerTest("Function with param int pointer to int pointer", &test_function_with_all_pointer_apply);
 }
 
 
@@ -41,6 +44,20 @@ double double_int(int a)
 int func_param_int_pointer(int* a)
 {
 	return a[0];
+}
+
+int* func_return_int_pointer(int a)
+{
+	int* arr = new int[1];
+	arr[0] = a;
+	return arr;
+}
+
+int* func_all_int_pointer(int* a)
+{
+	int* arr = new int[1];
+	arr[0] = a[0];
+	return arr;
 }
 
 
@@ -75,4 +92,34 @@ void test_function_with_func_param_int_pointer()
 	assertEqual(func(a), 0);
 	assertEqual(func(b), 1);
 	assertNotEqual(func(b), 0);
+}
+
+void test_function_with_return_pointer_apply()
+{
+	Function<int, int>::func_return_pointers func = func_return_int_pointer;
+
+	int* a = func(0);
+	int* b = func(1);
+
+	assertEqual(a[0], 0);
+	assertEqual(b[0], 1);
+	assertNotEqual(b[0], 0);
+}
+
+void test_function_with_all_pointer_apply()
+{
+	Function<int, int>::func_all_pointers func = func_all_int_pointer;
+
+	int* aa = new int[1];
+	aa[0] = 0;
+
+	int* bb = new int[1];
+	bb[0] = 1;
+
+	int* a = func(aa);
+	int* b = func(bb);
+
+	assertEqual(a[0], aa[0]);
+	assertEqual(b[0], bb[0]);
+	assertNotEqual(b[0], aa[0]);
 }
