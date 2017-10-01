8c71c5286e63fcdb04c833832dde44696704a24f
Apostolis <aldrigmej@yahoo.gr>
2017-10-01 12:45:50 +0300

    Created empty Mp3 parser class

---
+++ src/audio/mp3/Mp3Parser.cpp
@@ -0,0 +1,16 @@
+#include "Mp3Parser.h"
+
+Mp3Parser::Mp3Parser()
+{
+
+}
+
+Mp3Parser::~Mp3Parser()
+{
+
+}
+
+void Mp3Parser::parse()
+{
+	
+}
---
+++ src/audio/mp3/Mp3Parser.h
@@ -0,0 +1,18 @@
+#ifndef Mp3Parser_h__
+#define Mp3Parser_h__
+
+
+class Mp3Parser
+{
+private:
+	
+
+public:
+	Mp3Parser();
+	virtual ~Mp3Parser();
+
+	void parse();
+
+};
+
+#endif // Mp3Parser_h__
