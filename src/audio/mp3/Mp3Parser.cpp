aaf32e627bfad6307267934d7c6602ec66c8b0d6
Apostolis <aldrigmej@yahoo.gr>
2017-10-01 12:57:53 +0300

    Added Take Five song in resources for testing

--- src/audio/mp3/Mp3Parser.cpp
+++ src/audio/mp3/Mp3Parser.cpp
@@ -1,5 +1,18 @@
 #include "Mp3Parser.h"
 
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+
+using namespace std;
+
+/*
+	http://www.cplusplus.com/forum/general/105054/
+*/
+
+
 Mp3Parser::Mp3Parser()
 {
 
@@ -12,5 +25,7 @@ Mp3Parser::~Mp3Parser()
 
 void Mp3Parser::parse()
 {
-	
+	const char* filename = "03-TakeFive.mp3";
+
+	ifstream mp3file(filename, ios::binary);
 }
