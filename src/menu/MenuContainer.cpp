827bc098b53ac38f7fd43fecb0514618c046ac8c
Apostolos <apostolos@projectbeagle.com>
2017-10-11 17:56:52 +0300

    Added check method in menuItems in order to run before the action

--- src/menu/MenuContainer.cpp
+++ src/menu/MenuContainer.cpp
@@ -250,6 +250,8 @@ void MenuContainer::action()
 			{
 				logSrv->clearScreen();
 				logSrv->outString(selectedItem->identify());
+
+				selectedItem->check();
 				selectedItem->action();
 
 				if (this->continueQuestion)
