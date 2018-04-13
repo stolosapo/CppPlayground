cd5025ff53892ba5302ea359f485be96b2e78b41
Apostolos <apostolos@projectbeagle.com>
2018-04-13 15:08:15 +0300

    Comments to ALSA

--- src/kernel/audio/Alsa/AlsaCapture.h
+++ src/kernel/audio/Alsa/AlsaCapture.h
@@ -4,14 +4,29 @@
 #include <iostream>
 #include <string>
 
+#ifdef ALSA
+#include <alsa/asoundlib.h>
+#endif
+
 using namespace std;
 
 class AlsaCapture
 {
+private:
+
+#ifdef ALSA
+	snd_pcm_t *snd_handle;
+#endif
+
+	
+
 public:
 	AlsaCapture();
 	virtual ~AlsaCapture();
 
+	void initialize();
+
+	void playback();
         void capture();
 };
 
