812a270825da3f48e4e2dd77f9f54f427b583a26
Apostolos <aldrigmej@yahoo.gr>
2018-10-21 15:14:13 +0300

    More Lame

--- src/kernel/audio/encoding/lame/LameAudioEncodingService.cpp
+++ src/kernel/audio/encoding/lame/LameAudioEncodingService.cpp
@@ -1,9 +1,5 @@
 #include "LameAudioEncodingService.h"
 
-#ifdef LAME
-#include <lame/lame.h>
-#endif
-
 /*
 Example
 https://stackoverflow.com/questions/2495420/is-there-any-lame-c-wraper-simplifier-working-on-linux-mac-and-win-from-pure/2496831#2496831
@@ -24,6 +20,29 @@ void LameAudioEncodingService::encode(const char* filename)
 
 }
 
+int LameAudioEncodingService::mp3_readpcm(
+    const int audio_buffer_size,
+    int read_size,
+    unsigned char* audio_original_buffer,
+    short int* left,
+    short int* right)
+{
+    unsigned char buf[audio_buffer_size];
+    int samples = 0;
+
+    if (read_size <= 0)
+    {
+        return read_size;
+    }
+
+    while (!samples)
+    {
+        samples = decode(audio_original_buffer, read_size, left, right);
+    }
+
+    return samples;
+}
+
 int LameAudioEncodingService::decode(
     unsigned char* buffer,
     size_t bufflen,
@@ -33,11 +52,29 @@ int LameAudioEncodingService::decode(
     int res = 0;
 
 #ifdef LAME
-    // res = lame_decode(buffer, bufflen, left, right);
+    // res = hip_decode(buffer, bufflen, left, right);
 #endif
 
     return res;
 }
+
+#ifdef LAME
+int LameAudioEncodingService::reencode(
+    lame_t lame,
+    int samples,
+    short int* left,
+    short int* right,
+    unsigned char* audio_encoded_buffer,
+    int audio_encoded_buffer_len)
+{
+    return lame_encode_buffer(lame, left, right, samples, audio_encoded_buffer, audio_encoded_buffer_len);
+}
+
+int LameAudioEncodingService::flush(lame_t lame, unsigned char* audio_encoded_buffer, int max_len)
+{
+    return lame_encode_flush_nogap(lame, audio_encoded_buffer, max_len);
+}
+#endif
 
 int LameAudioEncodingService::test_encode(
     int samplerate,
--- src/kernel/audio/encoding/lame/LameAudioEncodingService.h
+++ src/kernel/audio/encoding/lame/LameAudioEncodingService.h
@@ -4,23 +4,48 @@
 #include <iostream>
 #include <string>
 
+#ifdef LAME
+#include <lame/lame.h>
+#endif
+
 #include "../AudioEncodingService.h"
 
 using namespace std;
 
 class LameAudioEncodingService: public AudioEncodingService
 {
+private:
+
+    int mp3_readpcm(
+        const int audio_buffer_size,
+        int read_size,
+        unsigned char* audio_original_buffer,
+        short int* left,
+        short int* right);
+
+    int decode(
+        unsigned char* buffer,
+        size_t bufflen,
+        short int* left,
+        short int* right);
+
+#ifdef LAME
+    int reencode(
+        lame_t lame,
+        int samples,
+        short int* left,
+        short int* right,
+        unsigned char* audio_encoded_buffer,
+        int audio_encoded_buffer_len);
+
+    int flush(lame_t lame, unsigned char* audio_encoded_buffer, int max_len);
+#endif
+
 public:
 	LameAudioEncodingService();
 	virtual ~LameAudioEncodingService();
 
 	virtual void encode(const char* filename);
-
-    virtual int decode(
-        unsigned char* buffer,
-        size_t bufflen,
-        short int* left,
-        short int* right);
 
     virtual int test_encode(
         int samplerate,
