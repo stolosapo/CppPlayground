aca309e0e071d3d24506dc4a2768abd8cc10872a
Apostolos <aldrigmej@yahoo.gr>
2020-11-14 22:18:32 +0200

    Code reformat

--- src/kernel/observer/Event.cpp
+++ src/kernel/observer/Event.cpp
@@ -12,87 +12,87 @@ Event::~Event()
 
 void Event::addEventHandler(EventHandler* eventHandler)
 {
-        eventHandlers.push_back(eventHandler);
+    eventHandlers.push_back(eventHandler);
 }
 
 void Event::addEventHandler(Delegate delegate)
 {
-        delegates.push_back(delegate);
+    delegates.push_back(delegate);
 }
 
 void Event::removeEventHandler(EventHandler* eventHandler)
 {
-        int count = eventHandlers.size();
-        int i;
+    int count = eventHandlers.size();
+    int i;
 
-        for (i = 0; i < count; i++)
+    for (i = 0; i < count; i++)
+    {
+        if(eventHandlers[i] == eventHandler)
         {
-                if(eventHandlers[i] == eventHandler)
-                {
-                        break;
-                }
+            break;
         }
+    }
 
-        if(i < count)
-        {
-                eventHandlers.erase(eventHandlers.begin() + i);
-        }
+    if (i < count)
+    {
+        eventHandlers.erase(eventHandlers.begin() + i);
+    }
 }
 
 void Event::removeEventHandler(Delegate delegate)
 {
-        int count = delegates.size();
-        int i;
+    int count = delegates.size();
+    int i;
 
-        for (i = 0; i < count; i++)
+    for (i = 0; i < count; i++)
+    {
+        if(delegates[i] == delegate)
         {
-                if(delegates[i] == delegate)
-                {
-                        break;
-                }
+            break;
         }
+    }
 
-        if(i < count)
-        {
-                delegates.erase(delegates.begin() + i);
-        }
+    if (i < count)
+    {
+        delegates.erase(delegates.begin() + i);
+    }
 }
 
 void Event::raise(void* sender, EventArgs* e)
 {
-        /* Iterate Event Handlers */
-        for (int i = 0; i < eventHandlers.size(); i++)
-        {
-                eventHandlers[i]->onEvent(sender, e);
-        }
+    /* Iterate Event Handlers */
+    for (int i = 0; i < eventHandlers.size(); i++)
+    {
+        eventHandlers[i]->onEvent(sender, e);
+    }
 
-        /* Iterate delegates */
-        for (int i = 0; i < delegates.size(); i++)
-        {
-                (delegates[i])(sender, e);
-        }
+    /* Iterate delegates */
+    for (int i = 0; i < delegates.size(); i++)
+    {
+        (delegates[i])(sender, e);
+    }
 }
 
 Event& Event::operator+=(EventHandler* eventHandler)
 {
-        addEventHandler(eventHandler);
-        return *this;
+    addEventHandler(eventHandler);
+    return *this;
 }
 
 Event& Event::operator+=(Delegate delegate)
 {
-        addEventHandler(delegate);
-        return *this;
+    addEventHandler(delegate);
+    return *this;
 }
 
 Event& Event::operator-=(EventHandler* eventHandler)
 {
-        removeEventHandler(eventHandler);
-        return *this;
+    removeEventHandler(eventHandler);
+    return *this;
 }
 
 Event& Event::operator-=(Delegate delegate)
 {
-        removeEventHandler(delegate);
-        return *this;
+    removeEventHandler(delegate);
+    return *this;
 }
