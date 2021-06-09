a2a81c851dfa5b62f994fda31b5fce48275be988
Apostolos <aldrigmej@yahoo.gr>
2021-06-09 19:15:03 +0300

    Abstract AudioSource Interface (#38)
    * Created AudioSource interface
    
    * Fixed indentation

---
+++ src/icecast/audio/AudioSourceConfig.h
@@ -0,0 +1,16 @@
+#ifndef AudioSourceConfig_h__
+#define AudioSourceConfig_h__
+
+#include <string>
+
+using namespace std;
+
+class AudioSourceConfig
+{
+public:
+    AudioSourceConfig();
+    virtual ~AudioSourceConfig();
+
+};
+
+#endif // AudioSourceConfig_h__
---
+++ src/icecast/audio/AudioSourceControls.h
@@ -0,0 +1,45 @@
+#ifndef AudioSourceControls_h__
+#define AudioSourceControls_h__
+
+#include <signal.h>
+
+#include "../../kernel/log/ILogService.h"
+#include "../../kernel/interruption/SignalService.h"
+
+using namespace std;
+
+class AudioSourceControls
+{
+private:
+    volatile sig_atomic_t _pause;
+    volatile sig_atomic_t _stop;
+    volatile sig_atomic_t _next;
+    volatile sig_atomic_t _shutdown;
+
+    ILogService *logSrv;
+    SignalService* sigSrv;
+
+protected:
+    void shutdown();
+    bool isShutdown();
+    void waitForResume();
+    void waitForShutdown();
+
+public:
+    AudioSourceControls(ILogService* logSrv, SignalService* sigSrv);
+    virtual ~AudioSourceControls();
+
+    void pause();
+    void resume();
+    void stop();
+    void play();
+    void next();
+    void normal();
+
+    bool isPaused();
+    bool isStoped();
+    bool isGoToNext();
+
+};
+
+#endif // AudioSourceControls_h__
